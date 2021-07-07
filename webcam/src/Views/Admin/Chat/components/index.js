3e9f3f5338b5e82e0a7955dd93ef9aa5cc609623
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-07 17:49:32 +0700

    Initial Function

---
+++ webcam/src/Views/Admin/Chat/components/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Chat(){
+    return(
+        <div>
+            <input name="sender"/>
+            <input name="message"/>
+        </div>
+    )
+}
+export default Chat
