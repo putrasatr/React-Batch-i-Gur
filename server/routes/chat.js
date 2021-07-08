5207caf0d6439831028599df08f1ecf4e3a03570
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-08 19:36:46 +0700

    Initial Chat Api

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const makeid = require('../helpers/makeid')
+const delFile = require('../helpers/deleteFile')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  try {
+    const data = await Chat.find({})
+    return res.status(200).json({
+      data,
+      message:"Success"
+    })
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({
+      data:false,
+      message:"Failed",
+    })
+  }
   res.render('index', { title: 'Express' });
 });
 
