88b743186e449f99bba86f7444b109a12672ad9b
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-04 08:44:18 +0700

    Initial router constant and maping router

--- webcam/src/Router/index.js
+++ webcam/src/Router/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import '../Assets/Css/mainStyle.css'
-import Login from "../Views/Login";
-import Users from "../Views/Users/";
-import Admin from "../Views/Admin";
 import Loading from "../Assets/Background/Loading";
 import MenuView from "../Assets/Background/MenuView";
 import { ProtectedRoute } from "../protected.route";
-import routers from "./router"
+import { routers } from "./router"
 function Routes() {
   const [isLoading, setLoading] = React.useState(true)
   setTimeout(() => setLoading(false), 2000)
@@ -17,10 +14,10 @@ function Routes() {
       {isLoading && <Loading />}
       <MenuView />
       <Switch>
-        {routers.map(({ path, Component, protected }, i) => {
-          protected
-            ? <ProtectedRoute key={i} path={path} component={Component} />
-            : <Route key={i} path="/welcome" exact component={Component} />
+        {routers.map(({ path, Component, protectedRoute }, i) => {
+          if (protectedRoute)
+            return <ProtectedRoute key={i} path={path} component={Component} />
+          return <Route key={i} path={path} exact component={Component} />
         })}
       </Switch>
     </>
--- webcam/src/Router/router.js
+++ webcam/src/Router/router.js
@@ -2,8 +2,8 @@ import Login from "../Views/Login";
 import Users from "../Views/Users/";
 import Admin from "../Views/Admin";
 
-export default [
-    { path: "/welcome", Component: Login, protected: false },
-    { path: "/admin", Component: Admin, protected: true },
-    { path: "/", Component: Users, protected: true }
+export const routers = [
+    { path: "/welcome", Component: Login, protectedRoute: false },
+    { path: "/admin", Component: Admin, protectedRoute: true },
+    { path: "/", Component: Users, protectedRoute: true }
 ]
