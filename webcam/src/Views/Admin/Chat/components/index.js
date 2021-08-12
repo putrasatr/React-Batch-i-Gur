8c34cd21e8023f9eb785ba7ac8f3d631045ff962
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-12 16:44:38 +0700

    modify Chat View

--- webcam/src/Views/Admin/Chat/components/index.js
+++ webcam/src/Views/Admin/Chat/components/index.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function Chat() {
+    const dispatch = useDispatch()
     const [value, setValue] = useState("")
+    const sender = useSelector(state => state.chat)
     const handleChangeContent = v => {
         setValue(v)
     }
+    useEffect(() => {
+        dispatch(chat(value, sender))
+        return () => {
+            clearTimeout()
+            dispatch(Chat())
+        }
+    }, [dispatch])
     return (
         <div>
             <input name="sender" />
